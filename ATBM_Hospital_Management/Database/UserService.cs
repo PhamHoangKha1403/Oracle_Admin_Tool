fe1d436a08eeece56bf07138b35e33ccb3564e24
PhamHoangKha1403 <phamhoangkha14032004@gmail.com>
2026-04-22 13:03:30 +0700

    feat: implement Oracle stored procedures for user management and data access and add UserService class for database interaction

--- ATBM_Hospital_Management/Database/UserService.cs
+++ ATBM_Hospital_Management/Database/UserService.cs
@@ -47,8 +47,8 @@ namespace ATBM_Hospital_Management.Database
             try
             {
                 OracleParameter[] p = {
-                    new OracleParameter("p_que_quan", OracleDbType.NVarchar2) { Value = string.IsNullOrEmpty(queQuan) ? DBNull.Value : (object)queQuan },
-                    new OracleParameter("p_sdt", OracleDbType.Varchar2) { Value = string.IsNullOrEmpty(sdt) ? DBNull.Value : (object)sdt }
+                    new OracleParameter("p_QUEQUAN", OracleDbType.NVarchar2) { Value = string.IsNullOrEmpty(queQuan) ? DBNull.Value : (object)queQuan },
+                    new OracleParameter("p_SODT", OracleDbType.Varchar2) { Value = string.IsNullOrEmpty(sdt) ? DBNull.Value : (object)sdt }
                 };
                 _db.ExecuteNonQuery("sp_NV_Update_NHANVIEN", p, CommandType.StoredProcedure);
             }
@@ -86,13 +86,13 @@ namespace ATBM_Hospital_Management.Database
             try
             {
                 OracleParameter[] p = {
-                    new OracleParameter("p_so_nha", OracleDbType.NVarchar2) { Value = string.IsNullOrEmpty(soNha) ? DBNull.Value : (object)soNha },
-                    new OracleParameter("p_ten_duong", OracleDbType.NVarchar2) { Value = string.IsNullOrEmpty(tenDuong) ? DBNull.Value : (object)tenDuong },
-                    new OracleParameter("p_quan_huyen", OracleDbType.NVarchar2) { Value = string.IsNullOrEmpty(quanHuyen) ? DBNull.Value : (object)quanHuyen },
-                    new OracleParameter("p_tinh_tp", OracleDbType.NVarchar2) { Value = string.IsNullOrEmpty(tinhTp) ? DBNull.Value : (object)tinhTp },
-                    new OracleParameter("p_tien_su_benh", OracleDbType.NVarchar2) { Value = string.IsNullOrEmpty(tienSuBenh) ? DBNull.Value : (object)tienSuBenh },
-                    new OracleParameter("p_tien_su_benh_gd", OracleDbType.NVarchar2) { Value = string.IsNullOrEmpty(tienSuBenhGd) ? DBNull.Value : (object)tienSuBenhGd },
-                    new OracleParameter("p_di_ung_thuoc", OracleDbType.NVarchar2) { Value = string.IsNullOrEmpty(diUngThuoc) ? DBNull.Value : (object)diUngThuoc }
+                    new OracleParameter("p_SONHA", OracleDbType.NVarchar2) { Value = string.IsNullOrEmpty(soNha) ? DBNull.Value : (object)soNha },
+                    new OracleParameter("p_TENDUONG", OracleDbType.NVarchar2) { Value = string.IsNullOrEmpty(tenDuong) ? DBNull.Value : (object)tenDuong },
+                    new OracleParameter("p_QUANHUYEN", OracleDbType.NVarchar2) { Value = string.IsNullOrEmpty(quanHuyen) ? DBNull.Value : (object)quanHuyen },
+                    new OracleParameter("p_TINHTP", OracleDbType.NVarchar2) { Value = string.IsNullOrEmpty(tinhTp) ? DBNull.Value : (object)tinhTp },
+                    new OracleParameter("p_TIENSUBENH", OracleDbType.NVarchar2) { Value = string.IsNullOrEmpty(tienSuBenh) ? DBNull.Value : (object)tienSuBenh },
+                    new OracleParameter("p_TIENSUBENHGD", OracleDbType.NVarchar2) { Value = string.IsNullOrEmpty(tienSuBenhGd) ? DBNull.Value : (object)tienSuBenhGd },
+                    new OracleParameter("p_DIUNGTHUOC", OracleDbType.NVarchar2) { Value = string.IsNullOrEmpty(diUngThuoc) ? DBNull.Value : (object)diUngThuoc }
                 };
                 _db.ExecuteNonQuery("sp_BN_Update_BENHNHAN", p, CommandType.StoredProcedure);
             }
